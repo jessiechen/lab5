042d31770538d6e99567022be13eb051561e41b9
vagrant <vagrant@precise32.(none)>
2014-02-23 07:42:46 +0000

    add friend

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -3,6 +3,11 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	$(".friends a").click(function(){
+		event.preventDefault();
+		var thisName=$(this).text();
+		$(this).text(anagrammedName(thisName));
+	});
 })
 
 /*
